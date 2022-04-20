eeb23b9ff3f574f8ac097f54a4b0789ccc3f4df4
Leo <Leoest011@gmail.com>
2022-04-19 21:15:05 -0400

    Update home.jsx
    Seconds counter Visual Component

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,42 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div
+			className="bigCounter"
+			style={{
+				background: "black",
+				color: "white",
+				margin: "50px",
+				display: "flex",
+				fontSize: "50px",
+				height: "50px",
+				lineHeight: "50px",
+			}}>
+			<div className="icon " style={{ marginLeft: "30px" }}>
+				<i className="fa-regular fa-clock-eight-thirty"></i>
+			</div>
+			<div className="six " style={{ marginLeft: "30px" }}>
+				{props.seconds[5]}
+			</div>
+			<div className="five" style={{ marginLeft: "30px" }}>
+				{props.seconds[4]}
+			</div>
+			<div className="four" style={{ marginLeft: "30px" }}>
+				{props.seconds[3]}
+			</div>
+			<div className="three" style={{ marginLeft: "30px" }}>
+				{props.seconds[2]}
+			</div>
+			<div className="two" style={{ marginLeft: "30px" }}>
+				{props.seconds[1]}
+			</div>
+			<div className="one" style={{ marginLeft: "30px" }}>
+				{props.seconds[0]}
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+
